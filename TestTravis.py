ec18a45c0a91c7cae8c2646d4cddf4733b1f6183
swissfiscal <swissfiscal@126.com>
2017-02-08 17:34:08 +0800

    abc

---
+++ TestTravis.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import time
+from selenium import webdriver
+desired_cap = {
+    'platform': "Mac OS X 10.9",
+    'browserName': "chrome",
+    'version': "31",
+}
+driver = webdriver.Remote(
+    command_executor='http://qazp:7a182ce9-a60c-4e08-b1c4-88123f2511fd@ondemand.saucelabs.com:80/wd/hub',
+    desired_capabilities=desired_cap)
+# driver.implicitly_wait(30)
+# driver.get('http://testadmin.yumimobi.com/index.php/user/login?t=dff78159f3417ea40c2466b8917ad317ff')
+# driver.quit()
+class TestTravis():
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+
+    def test_two(self):
+        # driver = webdriver.Chrome()
+        driver.implicitly_wait(30)
+        driver.get('http://testadmin.yumimobi.com/index.php/user/login?t=dff78159f3417ea40c2466b8917ad317ff')
+        driver.quit()
